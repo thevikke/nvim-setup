a4df45bb03da9246f378cb6433926fe425eb20c1
Kristian Tuusjärvi <kristian.tuusjarvi@visma.com>
2026-09-08 10:59:59 +0300

    feat(Markdown): Add markdown mermaid viewer to the setup.

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,5 +1,28 @@
+-- Treesitter parsers: :TSInstall markdown markdown_inline
+-- Renderer on PATH: npm i -g beautiful-mermaid-cli then which bm
+-- :checkhealth render-markdown-mermaid
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- mini.icons / nvim-web-devicons
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "echasnovski/mini.nvim", -- mini.icons
+    },
+    ft = { "markdown", "mdx" },
     opts = {},
+  },
+  {
+    "cavanaug/render-markdown-mermaid.nvim",
+    dependencies = {
+      "MeanderingProgrammer/render-markdown.nvim",
+    },
+    ft = { "markdown", "mdx" },
+    opts = {
+      mode = "unicode",
+      placement = "above",
+      replace = false,
+      debounce = 150,
+      auto_setup_render_markdown = false, -- you already set it up above
+    },
+  },
 }
