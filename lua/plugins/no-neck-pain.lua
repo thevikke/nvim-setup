1d90e80163bd931a55180e07fa3a17e9c552203b
Kristian Tuusjärvi <kristian.tuusjarvi@visma.com>
2026-09-08 13:58:08 +0300

    feat(NoNeckPain): Add package to center or focus screen size.

---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,11 @@
+return {
+  "shortcuts/no-neck-pain.nvim",
+  version = "*",
+  opts = {
+    width = 130, -- or "textwidth" / "colorcolumn"
+    mappings = {
+      enabled = true,
+      toggle = "<leader>np", -- default
+    },
+  },
+}
