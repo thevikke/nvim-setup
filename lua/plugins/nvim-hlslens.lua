0316c3b08914b37ddb05af9f0ca209bd6ea313f9
Kristian Tuusjärvi <kristian.tuusjarvi@visma.com>
2026-09-22 16:32:01 +0300

    feat(HighLightLensSearch): Add highlight lens to search.

---
+++ lua/plugins/nvim-hlslens.lua
@@ -0,0 +1,21 @@
+return {
+  "kevinhwang91/nvim-hlslens",
+  event = "VeryLazy",
+  opts = {}, -- or e.g. { calm_down = true, nearest_only = true }
+  config = function(_, opts)
+    require("hlslens").setup(opts)
+
+    local map = vim.keymap.set
+    local kopts = { noremap = true, silent = true }
+
+    map("n", "n", [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+    map("n", "N", [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+    map("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+    map("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+    map("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+    map("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+
+    -- Clear search.
+    map("n", "<Leader>l", "<Cmd>noh<CR>", kopts)
+  end,
+}
